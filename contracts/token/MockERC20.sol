a34500ced4517a1f6349fe67c27fccf8d3cb2cb2
Marvin <development@polkaex.io>
2022-02-24 00:11:43 +0800

    Update annotation.

--- contracts/token/MockERC20.sol
+++ contracts/token/MockERC20.sol
@@ -12,7 +12,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  */
 contract MockERC20 is ERC20 {
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        _mint(msg.sender, 18 * 10 ** decimals()); //token decimals
+        _mint(msg.sender, 18 * 10 ** decimals()); //Transfer token to deployer
     }
 
     function mint(address account, uint256 amount) external {
